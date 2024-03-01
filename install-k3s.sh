f26e3c696cf7a65ef6fb48d824c8bfce6ff77d18
code7a <115498258+code7a@users.noreply.github.com>
2024-02-29 23:37:35 -0500

    23.5.10 bins

--- install-k3s.sh
+++ install-k3s.sh
@@ -6,9 +6,16 @@ source .bash_profile
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 rm -rf /etc/machine-id; systemd-machine-id-setup;
 service k3s restart
-#get illumio-values.yaml
-pce="$(echo $(hostname) | cut -d. -f1).snc.$(echo $(hostname) | cut -d. -f2-4).$(echo $(hostname) | cut -d. -f6-8)"
-curl $pce/illumio-values.yaml -o illumio-values.yaml
+#get pce fqdn from hostname, check if dev or not
+if [[ "$(hostname)" == *".dev."* ]]; then
+ pce="$(echo $(hostname) | cut -d. -f1).snc.$(echo $(hostname) | cut -d. -f2-4).$(echo $(hostname) | rev | cut -d. -f1-3 | rev)"
+else
+ pce="$(echo $(hostname) | cut -d. -f1).snc.$(echo $(hostname) | cut -d. -f2-4).$(echo $(hostname) | rev | cut -d. -f1-2 | rev)"
+fi
+#get/set version variable
+[[ "$version" ]] || version=latest
+#get illumio-values-$version.yaml
+curl $pce/illumio-values-$version.yaml -o illumio-values-$version.yaml
 #append chain to ca bundle
 curl $pce/cert.pem -o /cert.crt
 curl $pce/chain.pem -o /chain.crt
@@ -24,7 +31,11 @@ update-ca-trust enable && update-ca-trust extract
 #helm install
 kubectl create ns illumio-system
 kubectl --namespace illumio-system create configmap root-ca-config --from-file=/etc/pki/ca-trust/source/anchors/cert.crt
-helm install illumio -f illumio-values.yaml oci://quay.io/illumio/illumio --namespace illumio-system
+if [[ "$version" == "4.3.0" ]]; then
+ helm install illumio -f illumio-values-$version.yaml oci://quay.io/illumio/illumio --namespace illumio-system --version 4.3.0
+else
+ helm install illumio -f illumio-values-$version.yaml oci://quay.io/illumio/illumio --namespace illumio-system
+fi
 #create nginx deployment
 kubectl create deployment nginx-alpha --image=nginx
 kubectl create service nodeport nginx-alpha --tcp=80:80
